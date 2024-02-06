b013dbe40d51738df839baea782a0b5f5ae4c6eb
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-06 17:13:05 +0530

    80

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,12 +1,20 @@
-import Link from "next/link";
+import Link from "next/link"
+import Links from "./links/Links"
+import styles from "./navbar.module.css"
+import { auth } from "@/lib/auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+
+  const session = await auth();
+
   return (
-    <div>
-      <div>logo</div>
-      <div></div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>Logo</Link>
+      <div>
+        <Links session={session}/>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
